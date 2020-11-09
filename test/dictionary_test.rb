87deb93a36425ccbc95518a2d7e61a41933df83d
Robert Heath <robert.b.heathii@gmail.com>
2020-11-09 14:40:44 -0600

    Test translate

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -10,12 +10,19 @@ class DictionaryTest < Minitest::Test
     assert_instance_of Dictionary, dictionary
     assert_equal Hash, dictionary.letter_equivalents.class
     assert_equal 27, dictionary.letter_equivalents.size
-    assert_equal '.0\n0.\n..', dictionary.letter_equivalents['z']
+    assert_equal ".0\n0.\n..", dictionary.letter_equivalents['z']
   end
 
   def test_convert
     dictionary = Dictionary.new
 
-    assert_equal '.0\n0.\n..', dictionary.convert('z')
+    assert_equal ".0\n0.\n..", dictionary.convert("z")
+  end
+
+  def test_translate
+    dictionary = Dictionary.new
+
+    expected = ".0.0....\n00.0..00\n0000..00"
+    assert_equal expected, dictionary.translate("ab c")
   end
 end
